4c2b51bb5c98c82217f42cb9a405b335f01af656
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-01-09 08:17:55 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from pykakasi import kakasi as ka
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    text = "形態素解析"
+
+    # オブジェクトをインスタンス化
+    kakasi = ka()
+    # モードの設定：J(Kanji) to H(Hiragana)
+    kakasi.setMode('J', 'H') 
+
+    # 変換して出力
+    conv = kakasi.getConverter()
+    text2 = conv.do(text)
+    return text2
+
+if __name__ == '__main__':
+    app.run()
