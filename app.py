9ff79a8bc726f777d3f6bac75895fc9f852488ec
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-01-09 22:37:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,8 @@ app = Flask(__name__, static_folder="./static", template_folder="./templates")
 
 @app.route('/')
 def index():
-
-    text = "形態素解析sann"
-
-    # オブジェクトをインスタンス化
-    kakasi = ka()
-    # モードの設定：J(Kanji) to H(Hiragana)
-    kakasi.setMode('J', 'H') 
-
-    # 変換して出力
-    conv = kakasi.getConverter()
-    text2 = conv.do(text)
-    return text2
-
+    name = "Hoge"
+    return render_template('hello.html', name=name) 
 
 
 
