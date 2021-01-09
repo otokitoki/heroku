18295ae7f2812134e48f0b0e4af4b36993d07f1a
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-01-09 18:14:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template 
 from pykakasi import kakasi as ka
 
 app = Flask(__name__)
@@ -19,5 +19,29 @@ def index():
     text2 = conv.do(text)
     return text2
 
-if __name__ == '__main__':
-    app.run()
+
+
+
+@app.route('/hello')
+def hello():
+    name = "Hoge"
+    return render_template('hello.html', name=name) 
+
+@app.route('/rolling_ryu')
+def rolling_ryu():
+    
+    return render_template('rolling_ryu.html') 
+
+@app.route('/localStorage')
+def localStorage():
+    
+    return render_template('localStorage.html') 
+
+@app.route('/morsecode_translate')
+def morsecode_translate():
+    
+    return render_template('morsecode_translate.html') 
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
