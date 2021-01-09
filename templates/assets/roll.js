b9a14502bb750f1e1d26b8d487193c9901abf827
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-01-09 18:42:13 +0900

    Add files via upload

---
+++ templates/assets/roll.js
@@ -0,0 +1,20 @@
+var $box = $('.box');
+var $button = $('.button');
+var isAnimate = false;
+
+$button.on('mousedown', function(){
+  if (isAnimate) {
+    return;
+  }
+  $('.rolling_image').attr('src', 'images/竜巻.jpg');
+  $button.val('アニメーション中');
+  $box.addClass('animation');
+  isAnimate = true;
+});
+
+$box.on('animationend', function(){
+  $button.val('アニメーション開始')
+  $box.removeClass('animation');
+  $('.rolling_image').attr('src', 'images/ryu.jpg');
+  isAnimate = false;
+});
