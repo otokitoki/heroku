093e34fec27a1e591681e4b4972dde4f2b6c33a3
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-01-09 20:31:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template 
 from pykakasi import kakasi as ka
 
-app = Flask(__name__)
+app = FlaskFlask(__name__, static_folder="./static", template_folder="./templates")
 
 @app.route('/')
 def index():
